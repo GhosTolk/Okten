762fc8e390f9cd3bc0bd09fab46ad8afaa1f4172
GhosTolk <ynadizhko@gmail.com>
2022-11-01 19:37:28 +0200

    finish

---
+++ JS/Les 1/main.js
@@ -0,0 +1,56 @@
+let s = "hello";
+console.log(s);
+
+let ss = "owu";
+console.log(ss);
+
+let sss = "com";
+console.log(sss);
+
+let ssss = "ua";
+console.log(ssss);
+
+let d = 1;
+console.log(d);
+
+let dd = 10;
+console.log(dd);
+
+let ddd = -999;
+console.log(ddd);
+
+let dddd = 123;
+console.log(dddd);
+
+let f = 3.14;
+console.log(f);
+
+let ff = 2.7;
+console.log(ff);
+
+let fff = 16;
+console.log(fff);
+
+let ffff = true;
+console.log(ffff);
+
+let g = false;
+console.log(g);
+
+
+let firstName = prompt("First Name?");
+let middleName = prompt("Middle Name?");
+let lastName = prompt("Last Name?");
+let age = prompt("Age?");
+
+let person = `${firstName} ${middleName} ${lastName} ${age}`;
+
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
