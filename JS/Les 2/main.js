640eeb554ed24685c2bc19e71561dd521d7b003b
GhosTolk <ynadizhko@gmail.com>
2022-11-03 16:25:14 +0200

    finish

---
+++ JS/Les 2/main.js
@@ -0,0 +1,57 @@
+let elem = ['first', 'second', 'third', false, true, 1, 2, 3, 4, {id: 10, name: 'Joe'}]
+console.log(elem[0]);
+console.log(elem[1]);
+console.log(elem[2]);
+console.log(elem[3]);
+console.log(elem[4]);
+console.log(elem[5]);
+console.log(elem[6]);
+console.log(elem[7]);
+console.log(elem[8]);
+console.log(elem[9]);
+
+
+let firstBook = {
+    title: 'JavaScript: The Definitive Guide',
+    pageCount: 150,
+    genre: 'IT',
+    authors: ['name', 'age']
+}
+
+let secondBook = {
+    title: 'Head First JavaScript Programming',
+    pageCount: 250,
+    genre: 'IT',
+    authors: ['name', 'age']
+}
+
+let thirdBook = {
+    title: 'JavaScript for Kids',
+    pageCount: 100,
+    genre: 'IT',
+    authors: ['name', 'age']
+}
+
+let users = [
+    {name: '', username: '', password: 'hsal;fh;hi'},
+    {name: '', username: '', password: 'hsal;fhi'},
+    {name: '', username: '', password: 'hsal;fh;hi'},
+    {name: '', username: '', password: 'hsal;h;hi'},
+    {name: '', username: '', password: 'hsal;fhi'},
+    {name: '', username: '', password: 'hsal;h;hi'},
+    {name: '', username: '', password: 'hsl;fh;hi'},
+    {name: '', username: '', password: 'hsal;fh;hi'},
+    {name: '', username: '', password: 'al;fh;hi'},
+    {name: '', username: '', password: 'hsal;f'}
+]
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
